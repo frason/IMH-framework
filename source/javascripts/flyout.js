4df818bc1397b8573a57aa8215f63e54d2cf701c
Matt Leigh <clensor@gmail.com>
2011-12-08 15:03:42 -0800

    adding config file and source dir

---
+++ source/javascripts/flyout.js
@@ -0,0 +1,74 @@
+// Global Variables
+var inprog = 0;
+var speed = '250';
+
+$(document).ready(function() {
+    //Flyout funtionality
+    // $("ul#leftNav li.categoryClosed").hover(function() {
+    //     //When mouse over ...
+    //     //Following event is applied to the flyout - makes it visible and then moves it to the right
+    //     //window.alert('this');
+    //     $(this).find("ul.flyout").css({
+    //         visibility: "visible"
+    //     });
+    //     $(this).find("ul.flyout").stop().animate({
+    //         left: 187,
+    //         opacity: '1'
+    //     },
+    //     {
+    //         queue: false,
+    //         duration: 200,
+    //         easing: 'easeOutExpo'
+    //     })
+    // });
+    // //
+    // $("ul#leftNav li.categoryClosed").mouseout(function() {
+    //     //When mouse out ...
+    //     //Following event is applied to the flyout - and moves it to the left makes it invisible
+    //     //window.alert('this');
+    //     $(this).find("ul.flyout").stop().animate({
+    //         left: -187,
+    //         opacity: '.01'
+    //     },
+    //     {
+    //         queue: false,
+    //         duration: 1,
+    //         easing: 'linear'
+    //     })
+    //     $(this).find("ul.flyout").css({
+    //         visibility: "hidden"
+    //     });
+    // });
+
+
+	// new
+	
+	$("ul#leftNav li.categoryClosed").hover(function() {
+		if (inprog == 0) {
+			inprog = 1;
+			$('.flyout', this).stop().animate({
+				left:187
+				// opacity:'1' handled by CSS now
+			}, {
+				queue: false,
+				duration: speed,
+				easing: 'easeOutExpo'
+			}).addClass('visible');
+		}
+	}, function() {
+		$('.flyout', this).animate({
+			left:-187
+			// opacity:'.01' handled by CSS now
+		}, {
+			queue: true,
+			duration: speed,
+			easing: 'easeOutExpo'
+		});
+		$('.flyout', this).removeClass('visible');
+		inprog = 0;
+	});
+	
+
+});
+
+
---
+++ source/javascripts/initialLoad.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function() {  
+		   $('#leftNav').css({top:-350, opacity:'.01'});
+           $('#leftNav').delay(500).animate({top:32, opacity:'1'},{duration:500, easing:'easeOutExpo'});
+           });  
