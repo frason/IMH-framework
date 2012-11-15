c71ce8b693bb5b43aef3038df565438499ca594b
Jason Frasier <jfrasier@gmail.com>
2012-11-14 17:29:19 -0800

    rewrote accordion to work with Accordion UI

--- source/javascripts/editorSidebarFunctions.js
+++ source/javascripts/editorSidebarFunctions.js
@@ -1,70 +1,29 @@
+var myAccord = function() {
+        $( "#accordion" ).accordion({
+            header: 'section',
+            heightStyle: "fill",
+            clearstyle:'true',
+            activate: function(event, ui) {
+            	//alert($(ui.oldHeader).html());
+            	$('header', ui.oldHeader).addClass('clickable');
+            	$('header', ui.newHeader).removeClass('clickable');
+            }
+
+       });
+
+}
+
+$(window).load(function() {
+	//draw_viewport();
+	myAccord();
+
+})
+
+
+
 $(document).ready(function() {
-	/* This is a way to animate the hover shift... but it seems laggy
 
-	$('div#accordion header.heading').mouseenter(function() {
-		if($(this).parent().hasClass('saved')&!($(this).parent().hasClass('current'))) {
-			
-			$(this).parent().animate({
-				"bottom": 5,
-				}, 250, function() { 
-					$(this).mouseleave(function() {
-						$(this).animate({"bottom": 0,}, 100);
-					});
-					//alert("yopu"); 
-				});
-		} else if($(this).parent().hasClass('content')&!($(this).parent().hasClass('current')) ){
-			//alert("in the ELSE IF");
-			$('section.saved').animate({
-				"bottom": 5,
-				}, 250, function() { 
-					$(this).mouseleave(function() {
-						$(this).animate({"bottom": 0,}, 100);
-					});
-					//alert("yopu"); 
-				});
-		};
-	})*/
-
-	
-
-
-	$('div#accordion header.heading').click(function() {
-		$(this).css('outline','none');
-		if($(this).parent().hasClass('saved')&!($(this).parent().hasClass('current'))) {
-			$('section').removeClass('hidden', 500);
-			var myPos = $(this).parent().position();
-			var myHeight = $('section.saved').height();
-			//alert("myPos.top is " + myPos.top + " myHeight is " + myHeight);
-			
-			$(this).parent().animate({
-				"bottom": myPos.top - myHeight,
-				}, 500, function() {
-					$(this).addClass('current');
-					$(this).removeClass('savedLoaded');
-					
-					$('header.heading').removeClass('clickable');
-					$('section.content').removeClass('current'); 
-					
-					$('section.content header').addClass('clickable'); 
-					$('section.content section').addClass('hidden');
-					//$('section.content > header.heading').css('cursor','pointer')
-				});
-	
-        } else if($(this).parent().hasClass('content')&!($(this).parent().hasClass('current')) ){  
-			$('section.saved').addClass('savedLoaded');
-			$('section.savedLoaded section').addClass('hidden');
-			$('section.content section').removeClass('hidden', 500);
-			$('section.saved').animate({"bottom": 0,}, 500);
-			$(this).removeClass('clickable');
-			$(this).addClass('current');
-			$('section.saved').removeClass('current'); 
-			$('section.saved').addClass('savedLoaded');
-			$('section.saved header').addClass('clickable'); 
-        }  
-        return false; 
-    });
-
-	$('ul li a').on({
+	$('.tabs ul li a').on({
 		
 		click: function(e) {
 			//alert("test");
@@ -82,7 +41,6 @@ $(document).ready(function() {
 		}
 		
 	});
-
 
 });
 
